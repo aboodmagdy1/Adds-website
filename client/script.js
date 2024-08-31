796963335d7c4a0718649159d7d303bf2b4dd862
Abood Magdy <c.aboodmagdy@gmail.com>
2024-08-31 09:18:47 +0300

    refactro: add some client code and style

--- client/script.js
+++ client/script.js
@@ -1,9 +1,10 @@
-const button = document.querySelector("button");
+const button = document.querySelector("#paybutton");
 const baseUrl = window.location.protocol + "//" + window.location.host;
 button.addEventListener("click", () => {
   fetch(`${baseUrl}/api/stripe/create-checkout-session`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
+    credentials: "include",
   })
     .then((res) => {
       if (res.ok) return res.json();
@@ -15,4 +16,31 @@ button.addEventListener("click", () => {
     .catch((e) => {
       console.log(e);
     });
+});
+
+const loginForm = document.querySelector("#loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#pass").value;
+
+  try {
+    const response = await fetch(`${baseUrl}/api/auth/signin`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Login failed");
+    }
+
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
 });
